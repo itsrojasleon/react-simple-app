43d6d2be4ac0fe49df249f04983633beda85a3db
rojasleon <rojasleon.dev@gmail.com>
2021-01-27 17:25:44 -0600

    bigger

--- src/App.tsx
+++ src/App.tsx
@@ -14,23 +14,23 @@ const App = () => {
     <Provider store={store}>
       <BrowserRouter>
         <Header />
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
         <main>
-          <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                Edit <code>src/App.js</code> and save to reload.
-              </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
-            </header>
-          </div>
           <Suspense fallback={<div>Loading...</div>}>
             <Switch>
               <Route path="/posts/:postId" component={Post} />
